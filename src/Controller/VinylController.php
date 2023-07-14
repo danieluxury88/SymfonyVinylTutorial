acc1bfde4a7df51e21322a07d84f7b8148a8d0bb
Daniel Proaño Chacon <danieluxury@gmail.com>
2023-07-14 07:22:57 -0500

    Add first twig file

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
 
     
-    #[Route('/home')]
+    #[Route('/')]
     public function homepage(): Response
     {
-        return new Response("Vinyls are excelent");
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfal', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a rouse', 'artist' => 'Seal'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey']
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route(path: '/browse/{slug}', name: 'browse', methods: ['GET'])]
